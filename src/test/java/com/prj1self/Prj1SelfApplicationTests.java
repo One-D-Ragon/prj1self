f7c68b311e57b9e015c98656919672fcfbd6ecb5
1dragon <dnjsdyd159@naver.com>
2024-05-07 11:37:58 +0900

    first commit

---
+++ src/test/java/com/prj1self/Prj1SelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prj1self;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prj1SelfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
